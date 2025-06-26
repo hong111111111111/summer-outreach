a5023aaae541e6ce9659c5a9ff4dd0f7c09bfa1a
hong111111111111 <ixtys0704@gmail.com>
2025-06-26 14:03:52 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
   projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
+  storageBucket: "YOUR_PROJECT.appspot.com",
   messagingSenderId: "YOUR_SENDER_ID",
   appId: "YOUR_APP_ID"
 };
