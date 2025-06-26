98ea5be78c8ae677d6876c365df66a73874bf541
hong111111111111 <ixtys0704@gmail.com>
2025-06-26 14:41:18 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
+// config.js
 export const firebaseConfig = {
   apiKey: "AIzaSyBNrqButtO47i3vr9p0V3WIHVPciPh5Pcg",
   authDomain: "summer-outreach.firebaseapp.com",
   projectId: "summer-outreach",
-  storageBucket: "summer-outreach.firebasestorage.app",
+  storageBucket: "summer-outreach.appspot.com",
   messagingSenderId: "737528313779",
   appId: "1:737528313779:web:15166f777331a9cf9b3dc6",
   measurementId: "G-1WG9S251RZ"
